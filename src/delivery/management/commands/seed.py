7eb06194cfce9676b39d7d382f02ba061e1dbae5
Pavel <pps.mayday@gmail.com>
2023-05-25 00:07:42 +0400

    Added seeding migrations command, update delivery models

---
+++ src/delivery/management/commands/seed.py
@@ -0,0 +1,81 @@
+import random
+from itertools import islice
+from csv import reader
+from decimal import Decimal
+
+from django.core.management import BaseCommand
+
+from delivery.models import Location, DeliveryCar
+from src.settings import BASE_DIR
+
+
+# 'Seed' mode for clearing and seeding data
+# 'Clear' mode only for clearing
+MODE_REFRESH = 'seed'
+MODE_CLEAR = 'clear'
+SEEDING_FILE_NAME = 'uszips_fortest.csv'
+
+
+class Command(BaseCommand):
+    help = "Seed database"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--mode', type=str, help="Mode")
+
+    def handle(self, *args, **options):
+        self.stdout.write('Seeding database...')
+        run_seed(self, options['mode'])
+        self.stdout.write('Seeding is done!')
+
+
+def create_locations():
+    """ Create database records (Location) from csv-file model: """
+
+    temp_locations_list = []
+    with open(f'{BASE_DIR}/{SEEDING_FILE_NAME}', encoding='utf-8') as seed_file:
+        data = islice(reader(seed_file), 1, None)
+        for row in data:
+
+            temp_locations_list.append(
+                Location(
+                    city=row[5],
+                    state=row[3],
+                    zip=row[0],
+                    latitude=Decimal(row[1]),
+                    longitude=Decimal(row[2]),
+                )
+            )
+        Location.objects.bulk_create(temp_locations_list, batch_size=999)
+
+
+def create_delivery_machines(number_of_machines: int = 10):
+    """ Create database records (DeliveryCar) """
+    possible_ids = get_possible_ids()
+
+    for i in range(number_of_machines):
+        new_car = DeliveryCar(
+                current_location=Location.objects.get(pk=random.choice(possible_ids)),
+                tonnage=random.randint(1000, 10001)
+            )
+        new_car.save()
+
+
+def run_seed(self, mode):
+    """ Run seeding database """
+    clear_data()
+    if mode == MODE_CLEAR:
+        return
+    create_locations()
+    create_delivery_machines()
+
+
+def clear_data():
+    """Delete all locations and cars from database"""
+    DeliveryCar.objects.all().delete()
+    Location.objects.all().delete()
+
+
+def get_possible_ids():
+    queryset = Location.objects.order_by('pk').all()
+    possible_ids = list(queryset.values_list('id', flat=True))
+    return possible_ids
--- src/delivery/models.py
+++ src/delivery/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class Location(models.Model):
 	city = models.CharField(max_length=30)
 	state = models.CharField(max_length=30)
-	zip = models.IntegerField()
+	zip = models.CharField()
 	latitude = models.DecimalField(max_digits=8, decimal_places=5, validators=[MinValueValidator(-90), MaxValueValidator(90)])
 	longitude = models.DecimalField(max_digits=8, decimal_places=5, validators=[MinValueValidator(-180), MaxValueValidator(180)])
 
@@ -18,12 +18,16 @@ class Cargo(models.Model):
 	delivery_location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, related_name='delivery')
 	weight = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(1000)])
 	description = models.TextField(max_length=1000)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 
 class DeliveryCar(models.Model):
 	car_id = models.CharField(editable=False, unique=True)
 	current_location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, related_name='current_position')
 	tonnage = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(1000)])
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 	def save(self, *args, **kwargs):
 		super().save(*args, **kwargs)
